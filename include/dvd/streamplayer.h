02ef641957b7cb3f2c4329076941b8be10290e98
Josh Strickland <js08k9487@gmail.com>
2017-06-25 14:51:43 -0500

    Implementing a StreamPlayer, inheriting QMediaPlayer and performing the cache dumping behaviors needed for when changing medial locations on the dvd, they will seem to happen quickly

---
+++ include/dvd/streamplayer.h
@@ -0,0 +1,11 @@
+#ifndef STREAMPLAYER_H
+#define STREAMPLAYER_H
+
+
+class StreamPlayer
+{
+public:
+    StreamPlayer();
+};
+
+#endif // STREAMPLAYER_H
---
+++ lib/dvd/streamplayer.cpp
@@ -0,0 +1,6 @@
+#include "../../include/dvd/streamplayer.h"
+
+StreamPlayer::StreamPlayer()
+{
+
+}
